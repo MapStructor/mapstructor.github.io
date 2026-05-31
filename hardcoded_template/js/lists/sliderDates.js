459e6ea139af2f691dc706fdbadcd2216f18042f
nittyjee <nittyjee@gmail.com>
2026-05-31 17:19:37 -0500

    Generic sliderDates

--- hardcoded_template/js/lists/sliderDates.js
+++ hardcoded_template/js/lists/sliderDates.js
@@ -1,8 +1,2 @@
-const sliderStartDate = "06/01/1852";
-const sliderEndDate   = "01/01/2025";
-
-/*
-//FOR DUTCH GRANTS TESTING
-const sliderStartDate = "06/01/1600";
-const sliderEndDate   = "01/01/1725";
-*/
+const sliderStartDate = "01/01/1900"; // Start of timeline slider (MM/DD/YYYY)
+const sliderEndDate   = "01/01/2025"; // End of timeline slider (MM/DD/YYYY)
